ca3c64df1f19844b70019ea918883ed00b203bbc
AlbinaRomanova <132841443+AlbinaRomanova@users.noreply.github.com>
2023-07-21 13:30:43 +0300

    решение Занятия 2

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,12 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (let key in salaries) {
+    const value = salaries[key];
+    if (typeof value === 'number' && Number.isFinite(value) && !Number.isNaN(value)) {
+      sum += value;
+    }
+  }
+  return sum;
 }
+
+
